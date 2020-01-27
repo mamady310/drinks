f295f794c5589688c4687c346eeaceaa64349372
Mamady Sillah <supurpisces310@msn.com>
2020-01-27 13:48:27 -0500

    populated information to DATABASES for drinks_app

--- drinks/settings.py
+++ drinks/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'drinks.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'dirnks_app',
+        'USER': 'drinksuser',
+        'PASSWORD': 'drinks1',
+        'HOST': 'localhost',
     }
 }
 
