16093229b6ad32269cc7c3cb2c96b79ef501ecbc
Mamady Sillah <supurpisces310@msn.com>
2020-01-27 15:23:23 -0500

    populated admin.py...then also did migrations for my Drink model

--- drinks_app/models.py
+++ drinks_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    spirit = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
