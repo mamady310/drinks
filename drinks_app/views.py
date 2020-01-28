66113597183c6d9140f990ce3581cea61c06577a
Mamady Sillah <supurpisces310@msn.com>
2020-01-28 14:35:03 -0500

    added recipe list to views.py functions

--- drinks_app/views.py
+++ drinks_app/views.py
@@ -5,4 +5,8 @@ from .models import Drink, Recipe, Comment
 def drink_list(request):
     drinks = Drink.objects.all()
     return render(request, 'drinks_app/drink_list.html', {'drinks': drinks})
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'drinks_app/recipe_list.html', {'recipes': recipes})    
                                                                                                                                                                                                                                  
